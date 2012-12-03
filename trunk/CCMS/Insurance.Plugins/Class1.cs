056efc58d5d734fe59f05e605db75574d7e3ab2f
chwangbjtu@gmail.com <chwangbjtu@gmail.com@0aceaf40-525c-163f-977e-c50002571c30>
2012-12-03 05:28:20 +0000

    新增保险管理

---
+++ trunk/CCMS/Insurance.Plugins/Class1.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Insurance.Plugins
+{
+    public class Class1
+    {
+    }
+}
---
+++ trunk/CCMS/Insurance.Plugins/Utils/BaseUserControl.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Insurance.Plugins.Utils
+{
+    public class BaseUserControl : CCMS.Plugin.AbstractPlugin
+    {
+        public BaseUserControl()
+        {
+            _appOrder = 2;
+            _appName = "保险管理";
+        }
+    }
+}
